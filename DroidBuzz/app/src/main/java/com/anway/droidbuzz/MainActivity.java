5b88428f4a2705db02882cc9d313369b3778573c
Anway <anwaysomani@gmail.com>
2019-09-06 10:46:34 +0530

    Initialised project

---
+++ DroidBuzz/app/src/main/java/com/anway/droidbuzz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.anway.droidbuzz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
